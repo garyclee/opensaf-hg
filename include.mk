1e61ca2f6fdde6a5c49c3e538c8f9a369ea1cb7e
Michael Bishop <michael.bishop@hp.com>
2009-04-02 10:22:30 -0600

    Minor enhancement to the hisv_events_demo to display fan and power supply events.
    No ticket.
    
    Signed-off-by: Michael Bishop <michael.bishop@hp.com>

---
+++ include.mk
@@ -0,0 +1,4 @@
+pkgsysconfdir = $(sysconfdir)/$(PACKAGE_NAME)
+pkglocalstatedir = $(localstatedir)/lib/$(PACKAGE_NAME)
+docdir = $(datadir)/doc/$(PACKAGE_NAME)-$(PACKAGE_VERSION)
+javadir = $(datadir)/java
