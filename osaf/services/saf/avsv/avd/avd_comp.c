garyclee/opensaf-hg

commit 8fccab2b034a9f9f3ce1bd4a79b20cc06ff13fb2

avsv/avd: properly update SU & SI states (#810)
- Update saAmfSUOperState in IMM depending on the hosting node state when a new SU is added
- Update saAmfSIAssignmentState in IMM when a new SI is added
- Calculate saAmfSIAssignmentState and update IMM according to table 11 in AMF spec
- Refactored a couple of macros to functions
- Changed samples/avsv/*.xml to be able to load both 2N & n-way active Demo at the same time

osaf/services/saf/avsv/avd/avd_imm.c
@@ -774,7 +774,7 @@ unsigned int avd_imm_config_get(void)
 
 done:
 	if (rc == NCSCC_RC_SUCCESS)
-		avd_log(NCSFL_SEV_NOTICE, "Configuration successfully read from IMM");
+		LOG_NO("AMF Configuration successfully read from IMM");
 	else
 		LOG_ER("Failed to read configuration, AMF will not start");
 
@@ -788,19 +788,20 @@ SaAisErrorT avd_saImmOiRtObjectUpdate(const SaNameT *dn, SaImmAttrNameT attribut
 	SaImmAttrModificationT_2 attrMod;
 	const SaImmAttrModificationT_2 *attrMods[] = { &attrMod, NULL };
 	SaImmAttrValueT attrValues[] = { value };
-	SaAisErrorT rc;
+	SaAisErrorT rc = SA_AIS_OK;
 
-	attrMod.modType = SA_IMM_ATTR_VALUES_REPLACE;
-	attrMod.modAttr.attrName = attributeName;
-	attrMod.modAttr.attrValuesNumber = 1;
-	attrMod.modAttr.attrValueType = attrValueType;
-	attrMod.modAttr.attrValues = attrValues;
+	if (avd_cb->avail_state_avd == SA_AMF_HA_ACTIVE) {
+		attrMod.modType = SA_IMM_ATTR_VALUES_REPLACE;
+		attrMod.modAttr.attrName = attributeName;
+		attrMod.modAttr.attrValuesNumber = 1;
+		attrMod.modAttr.attrValueType = attrValueType;
+		attrMod.modAttr.attrValues = attrValues;
 
-	rc = saImmOiRtObjectUpdate_2(avd_cb->immOiHandle, dn, attrMods);
+		rc = saImmOiRtObjectUpdate_2(avd_cb->immOiHandle, dn, attrMods);
 
-	if (rc != SA_AIS_OK)
-		avd_log(NCSFL_SEV_WARNING, "FAILED %u, '%s', %s",
-			rc, dn->value, attributeName);
+		if (rc != SA_AIS_OK)
+			LOG_ER("%s: FAILED %u, '%s', %s", __FUNCTION__, rc, dn->value, attributeName);
+	}
 
 	return rc;
 }
@@ -812,8 +813,7 @@ SaAisErrorT avd_saImmOiRtObjectCreate(const SaImmClassNameT className,
 		className, parentName, attrValues);
 
 	if (rc != SA_AIS_OK)
-		avd_log(NCSFL_SEV_WARNING, "FAILED %u, '%s'",
-			rc, parentName->value);
+		LOG_ER("%s: FAILED %u, '%s'", __FUNCTION__, rc, parentName->value);
 
 	return rc;
 }
@@ -823,8 +823,7 @@ SaAisErrorT avd_saImmOiRtObjectDelete(const SaNameT* dn)
 	SaAisErrorT rc = saImmOiRtObjectDelete(avd_cb->immOiHandle, dn);
 
 	if (rc != SA_AIS_OK)
-		avd_log(NCSFL_SEV_WARNING, "FAILED %u, '%s'",
-			rc, dn->value);
+		LOG_ER("%s: FAILED %u, '%s'", __FUNCTION__, rc, dn->value);
 
 	return rc;
 }

osaf/services/saf/avsv/avd/avd_su.c
@@ -493,6 +493,8 @@ static void su_add_to_model(AVD_SU *su)
 		node = avd_cb->ext_comp_info.local_avnd_node;
 	}
 
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_ADD(avd_cb, su, AVSV_CKPT_AVD_SU_CONFIG);
+
 	if (new_su && ((node->node_state == AVD_AVND_STATE_PRESENT) ||
 		       (node->node_state == AVD_AVND_STATE_NO_CONFIG) ||
 		       (node->node_state == AVD_AVND_STATE_NCS_INIT))) {
@@ -504,11 +506,13 @@ static void su_add_to_model(AVD_SU *su)
 			rc = SA_AIS_ERR_INVALID_PARAM;
 			goto done;
 		}
-	}
+
+		avd_su_oper_state_set(su, SA_AMF_OPERATIONAL_ENABLED);
+	} else
+		avd_su_oper_state_set(su, SA_AMF_OPERATIONAL_DISABLED);
 
 done:
 	assert(rc == NCSCC_RC_SUCCESS);
-	m_AVSV_SEND_CKPT_UPDT_ASYNC_ADD(avd_cb, su, AVSV_CKPT_AVD_SU_CONFIG);
 
 	/* Set runtime cached attributes. */
 	if (avd_cb->impl_set == TRUE) {
@@ -525,14 +529,9 @@ done:
 				&su->saAmfSUPresenceState);
 
 		avd_saImmOiRtObjectUpdate(&su->name,
-				"saAmfSUOperState", SA_IMM_ATTR_SAUINT32T,
-				&su->saAmfSUOperState);
-
-		avd_saImmOiRtObjectUpdate(&su->name,
 				"saAmfSUReadinessState", SA_IMM_ATTR_SAUINT32T,
 				&su->saAmfSuReadinessState);
 	}
-
 }
 
 SaAisErrorT avd_su_config_get(const SaNameT *sg_name, AVD_SG *sg)
@@ -1267,6 +1266,32 @@ static void su_ccb_apply_cb(CcbUtilOperationData_t *opdata)
 	}
 
 	TRACE_LEAVE();
+}
+
+void avd_su_inc_curr_act_si(AVD_SU *su)
+{
+	su->saAmfSUNumCurrActiveSIs++;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, su, AVSV_CKPT_SU_SI_CURR_ACTIVE);
+}
+
+void avd_su_dec_curr_act_si(AVD_SU *su)
+{
+	assert(su->saAmfSUNumCurrActiveSIs > 0);
+	su->saAmfSUNumCurrActiveSIs--;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, su, AVSV_CKPT_SU_SI_CURR_ACTIVE);
+}
+
+void avd_su_inc_curr_stdby_si(AVD_SU *su)
+{
+	su->saAmfSUNumCurrStandbySIs++;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, su, AVSV_CKPT_SU_SI_CURR_STBY);
+}
+
+void avd_su_dec_curr_stdby_si(AVD_SU *su)
+{
+	assert(su->saAmfSUNumCurrStandbySIs > 0);
+	su->saAmfSUNumCurrStandbySIs--;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, su, AVSV_CKPT_SU_SI_CURR_STBY);
 }
 
 void avd_su_constructor(void)

osaf/services/saf/avsv/avd/avd_sgNAcvRfsm.c
@@ -85,7 +85,7 @@ static AVD_SU *avd_sg_nacvred_su_chose_asgn(AVD_CL_CB *cb, AVD_SG *sg)
 	AVD_SUS_PER_SI_RANK *su_rank_rec;
 	AVD_SU_SI_REL *tmp_rel;
 
-	m_AVD_LOG_FUNC_ENTRY("avd_sg_nacvred_su_chose_asgn");
+	TRACE_ENTER2("'%s'", sg->name.value);
 
 	i_si = sg->list_of_si;
 	l_flag = TRUE;
@@ -143,8 +143,7 @@ static AVD_SU *avd_sg_nacvred_su_chose_asgn(AVD_CL_CB *cb, AVD_SG *sg)
 				/* Add the SU to the operation list */
 				avd_sg_su_oper_list_add(cb, i_su, FALSE);
 
-				/* Increment the SU count for the SI */
-				m_AVD_SI_INC_ACTV_CURR_SU(i_si);
+				avd_si_inc_curr_act_ass(i_si);
 
 				/* Check if the SI can take more assignments. If not exit the SU loop.                         
 				 */
@@ -200,8 +199,7 @@ static AVD_SU *avd_sg_nacvred_su_chose_asgn(AVD_CL_CB *cb, AVD_SG *sg)
 				/* Add the SU to the operation list */
 				avd_sg_su_oper_list_add(cb, i_su, FALSE);
 
-				/* Increment the SU count for the SI */
-				m_AVD_SI_INC_ACTV_CURR_SU(i_si);
+				avd_si_inc_curr_act_ass(i_si);
 
 				/* Check if the SI can take more assignments. If not exit the SU loop.
 				 */
@@ -568,11 +566,7 @@ uns32 avd_sg_nacvred_su_fault_func(AVD_CL_CB *cb, AVD_SU *su)
 
 uns32 avd_sg_nacvred_su_insvc_func(AVD_CL_CB *cb, AVD_SU *su)
 {
-
-	m_AVD_LOG_FUNC_ENTRY("avd_sg_nacvred_su_insvc_func");
-	m_AVD_LOG_RCVD_VAL(((long)su));
-
-	m_AVD_LOG_RCVD_VAL(su->sg_of_su->sg_fsm_state);
+	TRACE_ENTER2("'%s', %u", su->name.value, su->sg_of_su->sg_fsm_state);
 
 	/* An SU will not become in service when the SG is being locked or shutdown.
 	 */

osaf/services/saf/avsv/avd/avd_sgNWayfsm.c
@@ -307,10 +307,7 @@ uns32 avd_sg_nway_su_insvc_func(AVD_CL_CB *cb, AVD_SU *su)
 {
 	uns32 rc = NCSCC_RC_SUCCESS;
 
-	m_AVD_LOG_FUNC_ENTRY("avd_sg_nway_su_insvc_func");
-	m_AVD_LOG_RCVD_VAL(((long)su));
-
-	m_AVD_LOG_RCVD_VAL(su->sg_of_su->sg_fsm_state);
+	TRACE_ENTER2("'%s', %u", su->name.value, su->sg_of_su->sg_fsm_state);
 
 	/* An SU will not become in service when the SG is being locked or shutdown.
 	 */
@@ -1267,8 +1264,7 @@ uns32 avd_sg_nway_si_assign(AVD_CL_CB *cb, AVD_SG *sg)
 				avd_sg_su_oper_list_add(cb, curr_su, FALSE);
 				m_AVD_SET_SG_FSM(cb, sg, AVD_SG_FSM_SG_REALIGN);
 
-				/* increment the standby assignment count for the si */
-				m_AVD_SI_INC_STDBY_CURR_SU(curr_si);
+				avd_si_inc_curr_stdby_ass(curr_si);
 
 				/* verify if si needs more assigments */
 				if (m_AVD_SI_STDBY_CURR_SU(curr_si) == m_AVD_SI_STDBY_MAX_SU(curr_si))
@@ -1307,8 +1303,7 @@ uns32 avd_sg_nway_si_assign(AVD_CL_CB *cb, AVD_SG *sg)
 				avd_sg_su_oper_list_add(cb, curr_su, FALSE);
 				m_AVD_SET_SG_FSM(cb, sg, AVD_SG_FSM_SG_REALIGN);
 
-				/* increment the standby assignment count for the si */
-				m_AVD_SI_INC_STDBY_CURR_SU(curr_si);
+				avd_si_inc_curr_stdby_ass(curr_si);
 
 				/* verify if si needs more assigments */
 				if (m_AVD_SI_STDBY_CURR_SU(curr_si) == m_AVD_SI_STDBY_MAX_SU(curr_si))

osaf/services/saf/avsv/avd/avd_sgNoRfsm.c
@@ -572,11 +572,7 @@ uns32 avd_sg_nored_su_fault_func(AVD_CL_CB *cb, AVD_SU *su)
 
 uns32 avd_sg_nored_su_insvc_func(AVD_CL_CB *cb, AVD_SU *su)
 {
-
-	m_AVD_LOG_FUNC_ENTRY("avd_sg_nored_su_insvc_func");
-	m_AVD_LOG_RCVD_VAL(((long)su));
-
-	m_AVD_LOG_RCVD_VAL(su->sg_of_su->sg_fsm_state);
+	TRACE_ENTER2("'%s', %u", su->name.value, su->sg_of_su->sg_fsm_state);
 
 	/* An SU will not become in service when the SG is being locked or shutdown.
 	 */

osaf/services/saf/avsv/avd/avd_sgNpMfsm.c
@@ -1314,11 +1314,7 @@ uns32 avd_sg_npm_su_fault_func(AVD_CL_CB *cb, AVD_SU *su)
 
 uns32 avd_sg_npm_su_insvc_func(AVD_CL_CB *cb, AVD_SU *su)
 {
-
-	m_AVD_LOG_FUNC_ENTRY("avd_sg_npm_su_insvc_func");
-	m_AVD_LOG_RCVD_VAL(((long)su));
-
-	m_AVD_LOG_RCVD_VAL(su->sg_of_su->sg_fsm_state);
+	TRACE_ENTER2("'%s', %u", su->name.value, su->sg_of_su->sg_fsm_state);
 
 	/* An SU will not become in service when the SG is being locked or shutdown.
 	 */

osaf/services/saf/avsv/avd/avd_sgproc.c
@@ -60,7 +60,7 @@
 
 #include <avd.h>
 #include <avd_imm.h>
-#include <logtrace.h>
+#include <avd_su.h>
 
 /*****************************************************************************
  * Function: avd_new_assgn_susi
@@ -86,6 +86,7 @@
 uns32 avd_new_assgn_susi(AVD_CL_CB *cb, AVD_SU *su, AVD_SI *si,
 			 SaAmfHAStateT ha_state, NCS_BOOL ckpt, AVD_SU_SI_REL **ret_ptr)
 {
+	uns32 rc = NCSCC_RC_FAILURE;
 	AVD_SU_SI_REL *susi;
 	AVD_COMP_CSI_REL *compcsi;
 	NCS_BOOL l_flag;
@@ -96,7 +97,7 @@ uns32 avd_new_assgn_susi(AVD_CL_CB *cb, AVD_SU *su, AVD_SI *si,
 
 	if ((susi = avd_susi_create(cb, si, su, ha_state)) == NULL) {
 		avd_log(NCSFL_SEV_ERROR, "Could not create SUSI '%s' '%s'", su->name.value, si->name.value);
-		return NCSCC_RC_FAILURE;
+		goto done;
 	}
 
 	m_AVD_LOG_RCVD_VAL(((long)susi));
@@ -159,7 +160,7 @@ uns32 avd_new_assgn_susi(AVD_CL_CB *cb, AVD_SU *su, AVD_SI *si,
 		avd_compcsi_delete(cb, susi, TRUE);
 		/* Unassign the SUSI */
 		avd_susi_delete(cb, susi, TRUE);
-		return NCSCC_RC_FAILURE;
+		goto done;
 	}
 
 	/* Now send the message about the SU SI assignment to
@@ -168,14 +169,7 @@ uns32 avd_new_assgn_susi(AVD_CL_CB *cb, AVD_SU *su, AVD_SI *si,
 	 */
 
 	if (FALSE == ckpt) {
-		if (ha_state == SA_AMF_HA_ACTIVE) {
-			su->saAmfSUNumCurrActiveSIs++;
-		} else {
-			su->saAmfSUNumCurrStandbySIs++;
-		}
-
-		if (avd_snd_susi_msg(cb, su, susi, AVSV_SUSI_ACT_ASGN)
-		    != NCSCC_RC_SUCCESS) {
+		if (avd_snd_susi_msg(cb, su, susi, AVSV_SUSI_ACT_ASGN) != NCSCC_RC_SUCCESS) {
 			m_AVD_LOG_INVALID_VAL_ERROR(((uns32)ha_state));
 			m_AVD_LOG_INVALID_VAL_ERROR(((long)susi));
 			m_AVD_LOG_INVALID_NAME_VAL_ERROR(su->name.value, su->name.length);
@@ -184,36 +178,27 @@ uns32 avd_new_assgn_susi(AVD_CL_CB *cb, AVD_SU *su, AVD_SI *si,
 			avd_compcsi_delete(cb, susi, TRUE);
 			/* Unassign the SUSI */
 			avd_susi_delete(cb, susi, TRUE);
-			if (ha_state == SA_AMF_HA_ACTIVE) {
-				assert(su->saAmfSUNumCurrActiveSIs > 0);
-				su->saAmfSUNumCurrActiveSIs--;
-			} else {
-				assert(su->saAmfSUNumCurrStandbySIs > 0);
-				su->saAmfSUNumCurrStandbySIs--;
-			}
 
-			return NCSCC_RC_FAILURE;
+			goto done;
 		}
 
-		if (si->saAmfSINumCurrActiveAssignments > 0) {
-			si->saAmfSIAssignmentState = SA_AMF_ASSIGNMENT_FULLY_ASSIGNED;
-		}
-		if (ha_state == SA_AMF_HA_ACTIVE) {
-			avd_saImmOiRtObjectUpdate(&si->name, "saAmfSIAssignmentState",
-						  SA_IMM_ATTR_SAUINT32T, &si->saAmfSIAssignmentState);
-		}
+		if (ha_state == SA_AMF_HA_ACTIVE)
+			avd_su_inc_curr_act_si(su);
+		else
+			avd_su_inc_curr_stdby_si(su);
 
 		m_AVSV_SEND_CKPT_UPDT_ASYNC_ADD(cb, susi, AVSV_CKPT_AVD_SI_ASS);
 		m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(cb, su, AVSV_CKPT_AVD_SU_CONFIG);
-		if (susi->si->list_of_sisu->si_next == AVD_SU_SI_REL_NULL) {
+
+		if (susi->si->list_of_sisu->si_next == AVD_SU_SI_REL_NULL)
 			avd_gen_si_oper_state_chg_ntf(cb, susi->si);
-		}
-		avd_gen_su_ha_state_changed_ntf(cb, susi);
 	}
 
 	*ret_ptr = susi;
-	return NCSCC_RC_SUCCESS;
-
+	rc = NCSCC_RC_SUCCESS;
+done:
+	TRACE_LEAVE();
+	return rc;
 }
 
 /*****************************************************************************
@@ -880,18 +865,13 @@ void avd_ncs_su_mod_rsp(AVD_CL_CB *cb, AVD_AVND *avnd, AVSV_N2D_INFO_SU_SI_ASSIG
 
 void avd_su_si_assign_func(AVD_CL_CB *cb, AVD_EVT *evt)
 {
-
-	AVD_DND_MSG *n2d_msg;
+	AVD_DND_MSG *n2d_msg = evt->info.avnd_msg;
 	AVD_AVND *avnd, *su_node_ptr = NULL;
 	AVD_SU *su = NULL;
 	AVD_SU_SI_REL *susi;
 	NCS_BOOL q_flag = FALSE, qsc_flag = FALSE;
 
-	TRACE_ENTER();
-
-	assert(evt->info.avnd_msg);
-
-	n2d_msg = evt->info.avnd_msg;
+	TRACE_ENTER2("%x", n2d_msg->msg_info.n2d_su_si_assign.node_id);
 
 	m_AVD_LOG_MSG_DND_DUMP(NCSFL_SEV_DEBUG, n2d_msg, sizeof(AVD_DND_MSG), n2d_msg);
 
@@ -1150,15 +1130,11 @@ void avd_su_si_assign_func(AVD_CL_CB *cb, AVD_EVT *evt)
 		case AVSV_SUSI_ACT_DEL:
 			/* AvND can force a abrupt removal of assignments */
 			if (susi->state == SA_AMF_HA_STANDBY) {
-				assert(susi->su->saAmfSUNumCurrStandbySIs > 0);
-				susi->su->saAmfSUNumCurrStandbySIs--;
-				m_AVD_SI_DEC_STDBY_CURR_SU(susi->si);
-				m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(cb, susi->su, AVSV_CKPT_SU_SI_CURR_STBY);
+				avd_su_dec_curr_stdby_si(susi->su);
+				avd_si_dec_curr_stdby_ass(susi->si);
 			} else {
-				assert(susi->su->saAmfSUNumCurrActiveSIs > 0);
-				susi->su->saAmfSUNumCurrActiveSIs--;
-				m_AVD_SI_DEC_ACTV_CURR_SU(susi->si);
-				m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(cb, susi->su, AVSV_CKPT_SU_SI_CURR_ACTIVE);
+				avd_su_dec_curr_act_si(susi->su);
+				avd_si_dec_curr_act_ass(susi->si);
 			}
 			break;
 
@@ -1177,6 +1153,7 @@ void avd_su_si_assign_func(AVD_CL_CB *cb, AVD_EVT *evt)
 				m_AVD_LOG_INVALID_NAME_VAL_ERROR(susi->si->name.value, susi->si->name.length);
 				goto done;
 			}
+
 			if (n2d_msg->msg_info.n2d_su_si_assign.error == NCSCC_RC_SUCCESS) {
 				susi->fsm = AVD_SU_SI_STATE_ASGND;
 				m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(cb, susi, AVSV_CKPT_AVD_SI_ASS);
@@ -1220,24 +1197,18 @@ void avd_su_si_assign_func(AVD_CL_CB *cb, AVD_EVT *evt)
 					} else {
 						if (n2d_msg->msg_info.n2d_su_si_assign.ha_state == SA_AMF_HA_ACTIVE) {
 							if (susi->su->saAmfSUNumCurrStandbySIs != 0) {
-								susi->su->saAmfSUNumCurrActiveSIs++;
-								assert(susi->su->saAmfSUNumCurrStandbySIs > 0);
-								susi->su->saAmfSUNumCurrStandbySIs--;
-								m_AVD_SI_INC_ACTV_CURR_SU(susi->si);
-								m_AVD_SI_DEC_STDBY_CURR_SU(susi->si);
-								m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(cb, susi->su,
-												 AVSV_CKPT_AVD_SU_CONFIG);
+								avd_su_inc_curr_act_si(susi->su);
+								avd_su_dec_curr_stdby_si(susi->su);
+								avd_si_inc_curr_act_ass(susi->si);
+								avd_si_dec_curr_stdby_ass(susi->si);
 							}
 
 						} else if (n2d_msg->msg_info.n2d_su_si_assign.ha_state ==
 							   SA_AMF_HA_STANDBY) {
-							susi->su->saAmfSUNumCurrStandbySIs++;
-							assert(susi->su->saAmfSUNumCurrActiveSIs > 0);
-							susi->su->saAmfSUNumCurrActiveSIs--;
-							m_AVD_SI_INC_STDBY_CURR_SU(susi->si);
-							m_AVD_SI_DEC_ACTV_CURR_SU(susi->si);
-							m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(cb, susi->su,
-											 AVSV_CKPT_AVD_SU_CONFIG);
+							avd_su_dec_curr_act_si(susi->su);
+							avd_su_inc_curr_stdby_si(susi->su);
+							avd_si_inc_curr_stdby_ass(susi->si);
+							avd_si_dec_curr_act_ass(susi->si);
 						}
 					}
 
@@ -2315,14 +2286,14 @@ uns32 avd_sg_su_asgn_del_util(AVD_CL_CB *cb, AVD_SU *su, NCS_BOOL del_flag, NCS_
 
 				/* update the si counters */
 				if (SA_AMF_HA_ACTIVE == i_susi->state) {
-					m_AVD_SI_INC_ACTV_CURR_SU(i_susi->si);
-					m_AVD_SI_DEC_STDBY_CURR_SU(i_susi->si);
+					avd_si_inc_curr_act_ass(i_susi->si);
+					avd_si_dec_curr_stdby_ass(i_susi->si);
 				}
 
 				/* update the si counters */
 				if (SA_AMF_HA_ACTIVE == i_susi->state) {
-					m_AVD_SI_INC_ACTV_CURR_SU(i_susi->si);
-					m_AVD_SI_DEC_STDBY_CURR_SU(i_susi->si);
+					avd_si_inc_curr_act_ass(i_susi->si);
+					avd_si_dec_curr_stdby_ass(i_susi->si);
 				}
 
 				i_susi = i_susi->su_next;

osaf/services/saf/avsv/avd/avd_si.c
@@ -236,6 +236,8 @@ static void si_add_to_model(AVD_SI *si)
 	avd_app_add_si(si->si_on_app, si);
 	avd_sg_add_si(si->sg_of_si, si);
 	m_AVSV_SEND_CKPT_UPDT_ASYNC_ADD(avd_cb, si, AVSV_CKPT_AVD_SI_CONFIG);
+	avd_saImmOiRtObjectUpdate(&si->name, "saAmfSIAssignmentState",
+		SA_IMM_ATTR_SAUINT32T, &si->saAmfSIAssignmentState);
 }
 
 /**
@@ -788,6 +790,91 @@ static void si_ccb_apply_cb(CcbUtilOperationData_t *opdata)
 	}
 
 	TRACE_LEAVE();
+}
+
+/**
+ * Update the SI assignment state. See table 11 p.88 in AMF B.04
+ * IMM is updated aswell as the standby peer.
+ * @param si
+ */
+static void si_update_ass_state(AVD_SI *si)
+{
+	SaAmfAssignmentStateT newState;
+
+	if (si->saAmfSINumCurrActiveAssignments == 0)
+		newState = SA_AMF_ASSIGNMENT_UNASSIGNED;
+	else {
+		switch (si->sg_of_si->sg_on_sg_type->saAmfSgtRedundancyModel) {
+		case SA_AMF_2N_REDUNDANCY_MODEL:
+			/* fall through */
+		case SA_AMF_NPM_REDUNDANCY_MODEL:
+			if (si->saAmfSINumCurrActiveAssignments == 1 && si->saAmfSINumCurrStandbyAssignments == 1)
+				newState = SA_AMF_ASSIGNMENT_FULLY_ASSIGNED;
+			else
+				newState = SA_AMF_ASSIGNMENT_PARTIALLY_ASSIGNED;
+			break;
+		case SA_AMF_N_WAY_REDUNDANCY_MODEL:
+			if (si->saAmfSINumCurrActiveAssignments == 1 &&
+				si->saAmfSINumCurrStandbyAssignments == si->saAmfSIPrefStandbyAssignments)
+				newState = SA_AMF_ASSIGNMENT_FULLY_ASSIGNED;
+			else
+				newState = SA_AMF_ASSIGNMENT_PARTIALLY_ASSIGNED;
+			break;
+		case SA_AMF_N_WAY_ACTIVE_REDUNDANCY_MODEL:
+			if (si->saAmfSINumCurrActiveAssignments == si->saAmfSIPrefActiveAssignments &&
+				si->saAmfSINumCurrStandbyAssignments == 0)
+				newState = SA_AMF_ASSIGNMENT_FULLY_ASSIGNED;
+			else
+				newState = SA_AMF_ASSIGNMENT_PARTIALLY_ASSIGNED;
+			break;
+		case SA_AMF_NO_REDUNDANCY_MODEL:
+			if (si->saAmfSINumCurrActiveAssignments == 1 && si->saAmfSINumCurrStandbyAssignments == 0)
+				newState = SA_AMF_ASSIGNMENT_FULLY_ASSIGNED;
+			else
+				newState = SA_AMF_ASSIGNMENT_PARTIALLY_ASSIGNED;
+			break;
+		default:
+			assert(0);
+		}
+	}
+
+	if (newState != si->saAmfSIAssignmentState) {
+		/* Log? Send notification? */
+		si->saAmfSIAssignmentState = newState;
+		avd_saImmOiRtObjectUpdate(&si->name, "saAmfSIAssignmentState",
+								  SA_IMM_ATTR_SAUINT32T, &si->saAmfSIAssignmentState);
+		m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, si, AVSV_CKPT_SI_ASSIGNMENT_STATE);
+	}
+}
+
+void avd_si_inc_curr_act_ass(AVD_SI *si)
+{
+	si->saAmfSINumCurrActiveAssignments++;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, si, AVSV_CKPT_SI_SU_CURR_ACTIVE);
+	si_update_ass_state(si);
+}
+
+void avd_si_dec_curr_act_ass(AVD_SI *si)
+{
+	assert(si->saAmfSINumCurrActiveAssignments > 0);
+	si->saAmfSINumCurrActiveAssignments--;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, si, AVSV_CKPT_SI_SU_CURR_ACTIVE);
+	si_update_ass_state(si);
+}
+
+void avd_si_inc_curr_stdby_ass(AVD_SI *si)
+{
+	si->saAmfSINumCurrStandbyAssignments++;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, si, AVSV_CKPT_SI_SU_CURR_STBY);
+	si_update_ass_state(si);
+}
+
+void avd_si_dec_curr_stdby_ass(AVD_SI *si)
+{
+	assert(si->saAmfSINumCurrStandbyAssignments > 0);
+	si->saAmfSINumCurrStandbyAssignments--;
+	m_AVSV_SEND_CKPT_UPDT_ASYNC_UPDT(avd_cb, si, AVSV_CKPT_SI_SU_CURR_STBY);
+	si_update_ass_state(si);
 }
 
 void avd_si_constructor(void)

commit 5a4ee2e348f270673656678124bcbc0286899aff

avsv/avd: corrected logging issue introduced in #810 fix

osaf/services/saf/avsv/avd/avd_comp.c
@@ -426,21 +426,18 @@ static void comp_add_to_model(AVD_COMP *comp)
 		comp->su->si_max_standby = comp->max_num_csi_stdby;
 	}
 
-        /* Set runtime cached attributes. */
-        if (avd_cb->impl_set == TRUE) {
-		avd_saImmOiRtObjectUpdate(&comp->su->name,
-				"saAmfSUPreInstantiable", SA_IMM_ATTR_SAUINT32T,
-				&comp->su->saAmfSUPreInstantiable);
+	/* Set runtime cached attributes. */
+	avd_saImmOiRtObjectUpdate(&comp->su->name, "saAmfSUPreInstantiable",
+		SA_IMM_ATTR_SAUINT32T, &comp->su->saAmfSUPreInstantiable);
 
-		avd_saImmOiRtObjectUpdate(&comp->comp_info.name,
-				"saAmfCompReadinessState", SA_IMM_ATTR_SAUINT32T, &comp->saAmfCompReadinessState);
+	avd_saImmOiRtObjectUpdate(&comp->comp_info.name, "saAmfCompReadinessState",
+		SA_IMM_ATTR_SAUINT32T, &comp->saAmfCompReadinessState);
 
-		avd_saImmOiRtObjectUpdate(&comp->comp_info.name,
-				"saAmfCompOperState", SA_IMM_ATTR_SAUINT32T, &comp->saAmfCompOperState);
-
-		avd_saImmOiRtObjectUpdate(&comp->comp_info.name,
-				"saAmfCompPresenceState", SA_IMM_ATTR_SAUINT32T, &comp->saAmfCompPresenceState);
-	}
+	avd_saImmOiRtObjectUpdate(&comp->comp_info.name, "saAmfCompOperState",
+		SA_IMM_ATTR_SAUINT32T, &comp->saAmfCompOperState);
+	
+	avd_saImmOiRtObjectUpdate(&comp->comp_info.name, "saAmfCompPresenceState",
+		SA_IMM_ATTR_SAUINT32T, &comp->saAmfCompPresenceState);
 
 	m_AVSV_SEND_CKPT_UPDT_ASYNC_ADD(avd_cb, comp, AVSV_CKPT_AVD_COMP_CONFIG);
 }
@@ -1479,13 +1476,8 @@ static void comp_ccb_apply_delete_hdlr(struct CcbUtilOperationData *opdata)
 		comp->su->saAmfSUPreInstantiable = TRUE;
 	}
 
-	/* Set runtime cached attributes. */
-	if (avd_cb->impl_set == TRUE) {
-
-		avd_saImmOiRtObjectUpdate(&comp->su->name,
-				"saAmfSUPreInstantiable", SA_IMM_ATTR_SAUINT32T,
-				&comp->su->saAmfSUPreInstantiable);
-	}
+	avd_saImmOiRtObjectUpdate(&comp->su->name, "saAmfSUPreInstantiable", SA_IMM_ATTR_SAUINT32T,
+							  &comp->su->saAmfSUPreInstantiable);
 
 	/* send a message to the AVND deleting the
 	 * component.

osaf/services/saf/avsv/avd/avd_imm.c
@@ -632,7 +632,7 @@ SaAisErrorT avd_imm_impl_set(void)
 		}
 	}
 
-        avd_cb->impl_set = TRUE;
+	avd_cb->is_implementer = SA_TRUE;
 
 	TRACE_LEAVE2("%u", rc);
 	return rc;
@@ -790,7 +790,7 @@ SaAisErrorT avd_saImmOiRtObjectUpdate(const SaNameT *dn, SaImmAttrNameT attribut
 	SaImmAttrValueT attrValues[] = { value };
 	SaAisErrorT rc = SA_AIS_OK;
 
-	if (avd_cb->avail_state_avd == SA_AMF_HA_ACTIVE && avd_cb->immOiHandle) {
+	if ((avd_cb->avail_state_avd == SA_AMF_HA_ACTIVE) && avd_cb->is_implementer) {
 		attrMod.modType = SA_IMM_ATTR_VALUES_REPLACE;
 		attrMod.modAttr.attrName = attributeName;
 		attrMod.modAttr.attrValuesNumber = 1;

osaf/services/saf/avsv/avd/avd_role.c
@@ -425,7 +425,7 @@ static uns32 avd_role_switch_actv_qsd(AVD_CL_CB *cb, SaAmfHAStateT role)
 
 	/* Give up our IMM OI implementer role */
 	(void)immutil_saImmOiImplementerClear(cb->immOiHandle);
-	cb->impl_set = FALSE;
+	cb->is_implementer = FALSE;
 
 
 	/* Walk through all the nodes and stop AvND rcv HeartBeat. */

osaf/services/saf/avsv/avd/avd_su.c
@@ -522,24 +522,17 @@ static void su_add_to_model(AVD_SU *su)
 done:
 	assert(rc == NCSCC_RC_SUCCESS);
 
-	/* Set runtime cached attributes. */
-	if (avd_cb->impl_set == TRUE) {
-		avd_saImmOiRtObjectUpdate(&su->name,
-				"saAmfSUPreInstantiable", SA_IMM_ATTR_SAUINT32T,
-				&su->saAmfSUPreInstantiable);
+	avd_saImmOiRtObjectUpdate(&su->name, "saAmfSUPreInstantiable",
+		SA_IMM_ATTR_SAUINT32T, &su->saAmfSUPreInstantiable);
 
-		avd_saImmOiRtObjectUpdate(&su->name,
-				"saAmfSUHostedByNode", SA_IMM_ATTR_SANAMET,
-				&su->saAmfSUHostedByNode);
+	avd_saImmOiRtObjectUpdate(&su->name, "saAmfSUHostedByNode",
+		SA_IMM_ATTR_SANAMET, &su->saAmfSUHostedByNode);
 
-		avd_saImmOiRtObjectUpdate(&su->name,
-				"saAmfSUPresenceState", SA_IMM_ATTR_SAUINT32T,
-				&su->saAmfSUPresenceState);
+	avd_saImmOiRtObjectUpdate(&su->name, "saAmfSUPresenceState",
+		SA_IMM_ATTR_SAUINT32T, &su->saAmfSUPresenceState);
 
-		avd_saImmOiRtObjectUpdate(&su->name,
-				"saAmfSUReadinessState", SA_IMM_ATTR_SAUINT32T,
-				&su->saAmfSuReadinessState);
-	}
+	avd_saImmOiRtObjectUpdate(&su->name, "saAmfSUReadinessState",
+		SA_IMM_ATTR_SAUINT32T, &su->saAmfSuReadinessState);
 }
 
 SaAisErrorT avd_su_config_get(const SaNameT *sg_name, AVD_SG *sg)

osaf/services/saf/avsv/avd/include/avd_cb.h
@@ -256,7 +256,7 @@ typedef struct cl_cb_tag {
 	SaImmOiHandleT immOiHandle;
 	SaImmOiHandleT immOmHandle;
 	SaSelectionObjectT imm_sel_obj;
-	NCS_BOOL impl_set;
+	int is_implementer;
 
 	fmHandleT fm_hdl;
 	SaSelectionObjectT fm_sel_obj;